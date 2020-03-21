6063dd632588ce7d71f23e87d0702b6f6e9c8424
Rupesh8055 <rupeshbdsh@gmail.com>
2020-03-21 11:46:07 +0530

    initial setup

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,29 @@
+importScripts('./node_modules/firebase/firebase-app.js');
+importScripts('./node_modules/firebase/firebase-messaging.js');
+
+firebase.initializeApp({
+  apiKey: '{$ firebase.apiKey $}',
+  appId: '{$ firebase.appId $}',
+  authDomain: '{$ firebase.authDomain $}',
+  databaseURL: '{$ firebase.databaseURL $}',
+  messagingSenderId: '{$ firebase.messagingSenderId $}',
+  projectId: '{$ firebase.projectId $}',
+  storageBucket: '{$ firebase.storageBucket $}',
+});
+const messaging = firebase.messaging();
+
+messaging.setBackgroundMessageHandler(({ data }) => {
+  const notification = Object.assign({}, data, {
+    data: {
+      click_action: data.click_action,
+    },
+  });
+  return self.registration.showNotification(notification.title, notification);
+});
+
+self.addEventListener('notificationclick', event => {
+  const url = event.notification.data.click_action && event.notification.data.click_action.startsWith('/')
+    ? `${self.origin}${event.notification.data.click_action}`
+    : event.notification.data.click_action;
+  event.waitUntil(clients.openWindow(url));
+});
